c63411b058617d4ac04f27de522437ddc37f9b96
tohird03 <tohirjondoniyorov@gmail.com>
2023-01-27 15:04:24 +0500

    Add Article page

--- src/Pages/Article/Article.jsx
+++ src/Pages/Article/Article.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import ArticleCard from '../../Components/ArticleCard/ArticleCard';
 import Navbar from '../../Components/Navbar/Navbar';
-
+import "./Article.scss"
 const Article = () => {
     return (
         <>
@@ -10,8 +10,65 @@ const Article = () => {
             </header>
             <main>
                 <section className='article'>
-                    <div className="container">
-                        <ArticleCard />
+                    <div className="container article__container">
+                        <div className='article-sidebar'>
+                            <h3 className='article-sidebar__heading'>
+                                Все статьи
+                            </h3>
+                            <ul className='article-sidebar__list'>
+                                <li className='article-sidebar__item'>
+                                    <button className='article-sidebar__button'>
+                                        Интерфейсы
+                                    </button>
+                                </li>
+                                <li className='article-sidebar__item'>
+                                    <button className='article-sidebar__button'>
+                                        Интерфейсы
+                                    </button>
+                                </li>
+                                <li className='article-sidebar__item'>
+                                    <button className='article-sidebar__button'>
+                                        Интерфейсы
+                                    </button>
+                                </li>
+                                <li className='article-sidebar__item'>
+                                    <button className='article-sidebar__button'>
+                                        Интерфейсы
+                                    </button>
+                                </li>
+                                <li className='article-sidebar__item'>
+                                    <button className='article-sidebar__button'>
+                                        Интерфейсы
+                                    </button>
+                                </li>
+                                <li className='article-sidebar__item'>
+                                    <button className='article-sidebar__button'>
+                                        Интерфейсы
+                                    </button>
+                                </li>
+                                <li className='article-sidebar__item'>
+                                    <button className='article-sidebar__button'>
+                                        Интерфейсы
+                                    </button>
+                                </li>
+                                <li className='article-sidebar__item'>
+                                    <button className='article-sidebar__button'>
+                                        Интерфейсы
+                                    </button>
+                                </li>
+                            </ul>
+                        </div>
+                        <div className='article__cards'>
+                            <ArticleCard />
+                            <ArticleCard />
+                            <ArticleCard />
+                            <ArticleCard />
+                            <ArticleCard />
+                            <ArticleCard />
+                            <ArticleCard />
+                            <ArticleCard />
+                            <ArticleCard />
+                        </div>
                     </div>
                 </section>
             </main>
